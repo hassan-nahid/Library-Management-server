96e8e209ab4950d7002b6cb2918692ccff991b43
hassan-nahid <scientisthassannahid@gmail.com>
2025-06-22 15:52:38 +0600

    bug fixed

--- src/modules/Books/book.controller.ts
+++ src/modules/Books/book.controller.ts
@@ -7,18 +7,42 @@ export const createBook = async (req: Request, res: Response): Promise<any> => {
 
         const { title, author, genre, isbn, description } = req.body;
 
-        if (
-            typeof title !== 'string' ||
-            typeof author !== 'string' ||
-            typeof genre !== 'string' ||
-            typeof isbn !== 'string' ||
-            (description && typeof description !== 'string')
-        ) {
-           return res.send({
+        const fields = { title, author, genre, isbn };
+        const errors: any = {};
+
+        for (const [key, value] of Object.entries(fields)) {
+            if (typeof value !== 'string') {
+                errors[key] = {
+                    message: `${key[0].toUpperCase() + key.slice(1)} must be a string`,
+                    name: "ValidatorError",
+                    kind: "type",
+                    path: key,
+                    value: value ?? null
+                };
+            }
+        }
+
+        if (description && typeof description !== 'string') {
+            errors.description = {
+                message: "Description must be a string if provided",
+                name: "ValidatorError",
+                kind: "type",
+                path: "description",
+                value: description
+            };
+        }
+
+        if (Object.keys(errors).length > 0) {
+            return res.json({
                 success: false,
-                message: "Title must be a string",
+                message: "Validation failed",
+                error: {
+                    name: "ManualValidationError",
+                    errors
+                }
             });
         }
+
 
         const book = new Book(req.body);
         const data = await book.save();
