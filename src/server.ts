b4299972e39afceaa8c25ed505fea5403e507364
hassan-nahid <scientisthassannahid@gmail.com>
2025-07-04 16:38:52 +0600

    cors url added

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,10 @@ dotenv.config()
 
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: "https://library-management-client-peach.vercel.app",
+  credentials: true, 
+}));
 app.use(express.json())
 
 // routes
